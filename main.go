406d28e51cf82c53d143e954bf02e2f29fbff648
t1m_shev <timofey82n@mail.ru>
2025-03-04 22:50:00 +0300

    added time showing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/lipgloss"
 )
@@ -18,7 +19,8 @@ type Logger struct {
 	Name         string
 	CurLogLevel  string
 	LevelsToShow []LogLevel
-	Output       io.Writer
+	Output       []io.Writer
+	IsColorized  bool
 	FmtConfig    []string
 }
 
@@ -32,24 +34,39 @@ func itemExists(slice []LogLevel, item string) bool {
 }
 func (Logger Logger) Println(level LogLevel, msg string) {
 	var final_output string
+
 	if itemExists(Logger.LevelsToShow, level.Name) {
 		for _, cfg := range Logger.FmtConfig {
+
 			switch cfg {
 			case "{level}":
 				final_output += level.Name
+
 			case "{msg}":
 				final_output += msg
+
 			case "{name}":
 				final_output += Logger.Name
+
+			case "{time}":
+				final_output += time.Now().Format("2006-01-02 15:04:05")
+
 			default:
 				final_output += cfg
 			}
 		}
+		
+		for i := 0; i < len(Logger.Output); i++ {
+			if Logger.Output[i] != os.Stdout || !Logger.IsColorized {
+				fmt.Fprintln(Logger.Output[i], final_output)
+			
+			} else {
+				fmt.Fprintln(Logger.Output[i], level.style.Render(final_output))
+			}
 
-		fmt.Fprintln(Logger.Output, level.style.Render(final_output))
-
-		if strings.Contains(level.Name, "fatal") || strings.Contains(level.Name, "FATAL") {
-			os.Exit(1)
+			if strings.Contains(level.Name, "fatal") || strings.Contains(level.Name, "FATAL") {
+				os.Exit(1)
+			}
 		}
 	}
 }
